a741c1a934b7903ad7d2a7975a8bf45999d70192
KennethWang1 <122188744+KennethWang1@users.noreply.github.com>
2025-08-02 11:00:15 -0400

    mongo db connection

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,27 @@ const http = require("http");
 const fs = require("fs");
 const cors = require('cors');
 const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+const MongoDBClient = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await MongoDBClient.connect();
+    await MongoDBClient.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await MongoDBClient.close();
+  }
+}
+run().catch(MongoDBClient.dir);
 
 const app = express();
 const port = process.env.PORT || 3000;
